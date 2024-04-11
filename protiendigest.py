33ced5bf1268f8dd9156ec0a5522be56fd552478
mukul-kandwal <mukul170@live.com>
2024-04-11 19:47:54 -0400

    removed comments

--- protiendigest.py
+++ protiendigest.py
@@ -1,35 +1,3 @@
-# from Bio import SeqIO
-# from pyteomics import parser
-#
-# def digest_peptides(filename,digest):
-#     list_peptides = []
-#     set_peptides = set()
-#     dict_peptides = dict()
-#     for record in SeqIO.parse(filename, "fasta"):
-#         protein_id = str(record.id)
-#         sequence = str(record.seq)
-#     # use pyteomics to generate digested peptides
-#         digested_peptides = parser.cleave(filename, digest)
-#         for pep in digested_peptides:
-#             list_peptides.append(pep)
-#             set_peptides.add(pep)
-#             if pep in dict_peptides.keys():
-#                 dict_peptides[pep] += 1
-#             else:
-#                 dict_peptides[pep] = 1
-#
-#     print("number of peptides overall:", len(list_peptides))
-#     print("number of peptide sequences", len(set_peptides))
-#     print("number of peptide sequences in dict", len(dict_peptides.keys()))
-#
-#     num_pep = 0
-#     for pep in dict_peptides.keys():
-#         if dict_peptides[pep] > 1:
-#             num_pep += 1
-#
-#     print(num_pep)
-
-
 import csv
 from pyteomics import parser
 
